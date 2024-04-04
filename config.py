60402a0ccce256cbd6a63c024ca6e5c3162c75ce
sivanandapvs01 <165928223+sivanandapvs01@users.noreply.github.com>
2024-04-04 12:18:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>you need to join POCKET FM TELUGU BACKUP to get files\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>you need to join the following channel to get files\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
